88090293bba3d9a0133fdceb7b77fab6877b572e
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-19 10:17:47 +0530

    initial_commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import NewsSlider from '../Widgets/NewsSlider/slider'
+
 const Home = () => {
     return (
         <div>
-            Home
+            <NewsSlider
+                type="featured"
+                start={3}
+                amount={12}
+                settings={{
+                    dots:true
+                }}
+            />
         </div>
     );
 };
